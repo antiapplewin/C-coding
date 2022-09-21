f8f85ede35f9fedad3a0357b1d73087a1b09dac8
antiapplewin <108814916+antiapplewin@users.noreply.github.com>
2022-09-21 18:35:46 +0900

    Add files via upload

---
+++ data type/Just data.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("정수 %d와 -%d\n", 153, 153);
+	printf("실수 %f와 -%f\n", 42.195, 42.195);
+	printf("문자 \'%c\'와 \'%c\'\n", 'A', 'a');
+	printf("문자열 \"%s\"와 \"%s\"\n", "A.I.", "마빈 민스키");
+	printf("%d, %o, %x", 255, 255, 255);
+	return 0;
+}
